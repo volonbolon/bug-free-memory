457a24ad48b2565ff3ac0d9d1da1dfca7cb5145e
Ariel Rodriguez <ariel@volonbolon.me>
2019-08-14 19:50:46 -0300

    Add gitignore

--- blockchain/index.js
+++ blockchain/index.js
@@ -1,6 +1,6 @@
 const Block = require('./block');
 
-class Blockchain {
+class Index {
     constructor() {
         this.chain = [Block.genesis()];
     }
@@ -47,4 +47,4 @@ class Blockchain {
     }
 }
 
-module.exports = Blockchain;
+module.exports = Index;
